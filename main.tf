f6b8501f06f5c4f1540059a2780d210e0a0a7b9e
raff-hashicorp <84794994+raff-hashicorp@users.noreply.github.com>
2023-03-02 13:30:36 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,6 +18,14 @@ resource "azurerm_resource_group" "example" {
   location = "West Europe"
 }
 
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+
 resource "azurerm_subnet" "example" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.example.name
