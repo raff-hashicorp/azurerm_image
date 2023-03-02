17b570d62a519f0d1df037ba072691a9e2cf9137
raff-hashicorp <84794994+raff-hashicorp@users.noreply.github.com>
2023-03-02 13:20:17 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,15 +18,30 @@ resource "azurerm_resource_group" "example" {
   location = "West Europe"
 }
 
+resource "azurerm_subnet" "example" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
 resource "azurerm_network_interface" "example" {
   name                = "test-nic"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
+  #ip_configuration {
+  #  name                          = "testconfiguration1"
+  #  private_ip_address_allocation = "Static"
+  #}
+  
   ip_configuration {
-    name                          = "testconfiguration1"
-    private_ip_address_allocation = "Static"
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
   }
+  
+  
 }
 
 resource "azurerm_virtual_machine" "example" {
