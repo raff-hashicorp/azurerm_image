c05b1aed0274da392aac379e2e4586374e98840e
raff-hashicorp <84794994+raff-hashicorp@users.noreply.github.com>
2023-03-02 12:29:02 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.45.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+}
+
+
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "West Europe"
